a31be89baa96f55e609fab2c2a23e4de98afea76
Elvis Wangari <elviswangare@gmail.com>
2025-09-29 21:31:16 +0300

    feat(components): add footer and partners sections

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import Footer from "@/components/footer";
 import { Header } from "@/components/header";
 import HeroSection from "@/components/hero-section";
 
@@ -6,6 +7,7 @@ export default function Home() {
     <>
       <Header />
       <HeroSection />
+      <Footer />
     </>
   );
 }
