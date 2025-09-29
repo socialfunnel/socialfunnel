746e351c867739f7598f2dd2b65fe1657b0f15e7
Elvis Wangari <elviswangare@gmail.com>
2025-09-29 19:42:46 +0300

    Add favicon image to the public directory

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,11 @@
-import { HeroHeader } from "@/components/header";
+import { Header } from "@/components/header";
+import HeroSection from "@/components/hero-section";
 
 export default function Home() {
   return (
-    <HeroHeader />
+    <>
+      <Header />
+      <HeroSection />
+    </>
   );
 }
